35758b0edc807ee5805599e5069db34e49529fc8
quash123 <mierzejewskimt@gmail.com>
2023-10-24 15:36:25 +0200

    Update Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -20,7 +20,7 @@ using Azure.Identity;
 using Microsoft.eShopWeb.Web.Pages;
 using Microsoft.FeatureManagement;
 using Microsoft.IdentityModel.Tokens;
-
+// Test 1
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddConsole();
